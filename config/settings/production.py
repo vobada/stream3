bfc7cfd810535e41012606e2125f0ce5b24097ad
vobada <avobada990@gmail.com>
2018-11-21 11:36:58 +0100

    Updated env

--- config/settings/production.py
+++ config/settings/production.py
@@ -18,18 +18,6 @@ DATABASES = {
 }
 # CACHES
 # ------------------------------------------------------------------------------
-CACHES = {
-    'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': env('REDIS_URL'),
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-            # Mimicing memcache behavior.
-            # http://niwinz.github.io/django-redis/latest/#_memcached_exceptions_behavior
-            'IGNORE_EXCEPTIONS': True,
-        }
-    }
-}
 
 # SECURITY
 # ------------------------------------------------------------------------------
