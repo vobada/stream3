1cf8ff7d0d6bfa43d662fdbddd8592cd0e4dd5f8
vobada <avobada990@gmail.com>
2018-11-21 11:33:48 +0100

    Updated env

--- config/settings/production.py
+++ config/settings/production.py
@@ -13,7 +13,7 @@ ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['https://vobada3.herok
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(ROOT_DIR, 'db.sqlite3'),
     }
 }
 # CACHES
